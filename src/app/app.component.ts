1d4e4b556d91573fb11458004d66f1e9d9e8bd36
huyquoc1210 <contact.huyquoc@gmail.com>
2025-05-04 10:11:48 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-first-angular-app';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  public onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
